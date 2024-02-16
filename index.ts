bd0198ca44f38b0131066f700dc042d396c636a7
Troy Appleby <troy.appleby@mths.ca>
2024-02-16 14:36:14 +0000

    init

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+World program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* By:      Troy. Appleby
+* Version: 1.0
+* Since:   2024-02-16
+*/ 
+
+console.log("Hello, World!")
+
+console.log("\nDone.")
